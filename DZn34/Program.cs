7fc93b878505c779edffce2165190ce16c64360e
Volodya <skochilovvladimir@yandex.ru>
2023-03-16 00:47:57 +0700

    n34

---
+++ DZn34/Program.cs
@@ -0,0 +1,22 @@
+﻿int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int [size];
+Random rand = new Random();
+for (int i = 0; i < size; i++)
+{
+     numbers[i] = rand.Next(99, 999);
+     System.Console.Write(numbers[i] + ", ");
+}
+int kol(int[] numbers)
+{
+    int kol = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            kol = kol + 1;
+        }
+    }
+    return kol;
+}
+
+Console.WriteLine(kol(numbers));
